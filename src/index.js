6e003e9bd7ed948e785535180cb39f0bc6ed8efd
rishabh-saha <rsaha130@gmail.com>
2017-09-30 23:17:19 +0530

    Implementing Time Travel

--- src/index.js
+++ src/index.js
@@ -64,11 +64,12 @@ class Game extends React.Component {
         squares: Array(9).fill(null),
       }],
       xIsNext: true,
+      stepNumber: 0
     };
   }
 
   handleClick(i){
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -79,20 +80,28 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
     });
   }
 
   render() {
     
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];   
     const winner = calculateWinner(current.squares);
   
     const moves = history.map((step, move) => {
       const desc = move ?'Move #' + move :'Game start';
       return (
-        <li>
+        <li key={move}>
           <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
         </li>
       );
@@ -117,12 +126,12 @@ class Game extends React.Component {
       < div className="game-board" >
       < Board 
           squares={current.squares}
-          onClick={(i) => this.handleClick(i)}
+          onClick={i => this.handleClick(i)}
       / >
       < /div>
     < div className="game-info" >
     < div > {status} < /div> 
-  < ol > { /* TODO */ } < /ol> 
+  < ol > {moves} < /ol> 
   < /div> 
   < /div>
     );
